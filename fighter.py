81c6f4edc87f6f063115e650b0eb9a6250afd9fe
SledTreeSavior <89948594+SledTreeSavior@users.noreply.github.com>
2022-06-13 01:00:11 -0400

    Add files via upload

--- fighter.py
+++ fighter.py
@@ -2,11 +2,15 @@ import pygame
 
 class Fighter():
     def __init__(self, x, y):
+        self.flip = False
         self.rect = pygame.Rect((x, y, 80, 180))
         self.vel_y = 0
         self.jump = False
+        self.attacking = False
+        self.attack_type = 0
+        self.health = 100
 
-    def move(self, screen_width, screen_height):
+    def move(self, screen_width, screen_height, surface, target):
         SPEED = 10
         GRAVITY = 2
         dx = 0
@@ -15,16 +19,28 @@ class Fighter():
         # get keypresses
         key = pygame.key.get_pressed()
 
-        # movement
-        if key[pygame.K_a]:
-            dx = -SPEED
-        if key[pygame.K_d]:
-            dx = +SPEED
+        # can only perform other actions if not currently attacking
+        if self.attacking == False:
 
-        # jump
-        if key[pygame.K_w] and self.jump == False:
-            self.vel_y = -30
-            self.jump = True
+            # movement
+            if key[pygame.K_a]:
+                dx = -SPEED
+            if key[pygame.K_d]:
+                dx = +SPEED
+
+            # jump
+            if key[pygame.K_w] and self.jump == False:
+                self.vel_y = -30
+                self.jump = True
+
+            # attack
+            if key[pygame.K_r] or key[pygame.K_t]:
+                self.attack(surface, target)
+                # determine which attack type was used
+                if key[pygame.K_r]:
+                    self.attack_type = 1
+                if key[pygame.K_t]:
+                    self.attack_type = 2
 
         # apply gravity
         self.vel_y += GRAVITY
@@ -35,15 +51,29 @@ class Fighter():
             dx = -self.rect.left
         if self.rect.right + dx > screen_width:
             dx = screen_width - self.rect.right
-        # the floor int eh bg_image is 110 pixels tall
+        # the floor in the bg_image is 110 pixels tall
         if self.rect.bottom + dy > screen_height - 110:
             self.vely_y = 0
             self.jump = False
             dy = screen_height - 110 - self.rect.bottom
 
+        # ensure that the players are facing eachother
+        if target.rect.centerx > self.rect.centerx:
+            self.flip = False
+        else:
+            self.flip = True
+
         # update player position
         self.rect.x += dx
         self.rect.y += dy
 
+    def attack(self, surface, target):
+        self.attacking = True
+        attacking_rect = pygame.Rect(self.rect.centerx - (2 * self.rect.width * self.flip), self.rect.y, 2 * self.rect.width, self.rect.height)
+        if attacking_rect.colliderect(target.rect):
+            target.health -= 10
+
+        pygame.draw.rect(surface, (0, 255, 0), attacking_rect)
+
     def draw(self, surface):
-        pygame.draw.rect(surface, (255, 0, 0), self.rect)
+        pygame.draw.rect(surface, (255, 0, 0), self.rect)      
